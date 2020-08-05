35797c8f6a98e51d74ab5bb093a43a479d69aa82
Marina8989 <marina.mill209@gmail.com>
2020-08-04 20:52:25 -0700

    working on  js part of the game

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-
+//make sure that html and js are loaded before running js
 if(document.readyState === 'loading') {
    document.addEventListener('DOMContentLoaded', ready());
 }else {
     ready();
 }
+class MixOrMatch{
+    constructor(totalTime, cards) {
+     this.totalTime = totalTime;
+     this.cardsArray = cards;
+     this.timeRemaining = totalTime;
+     this.timer = document.getElementById('time-remaining');
+     this.ticker = document.getElementById('flips');
+    }
+   // every time you start a new game, everythign resets
+    startGame() {
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+    }
+}
 
+
+//when the page is loaded (ready) you can fire js
 function ready() {
  
     let overlays = Array.from(document.getElementsByClassName("overlay-text"));
     let cards = Array.from(document.getElementsByClassName("card"));
 
-
+ //hide class visible to start the game
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
            overlay.classList.remove('visible');
         });
     })
+//on click of card trigget eventListener
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+
+        });
+    });
 }
 
